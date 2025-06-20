778e997d37de0e9b6921ac9628ef22c8fdaa5f8d
imsonukalwar <imsonukalwar@gmail.com>
2025-06-20 08:25:34 +0530

    Update comparision.js

--- comparision.js
+++ comparision.js
@@ -1,1 +1,29 @@
 console.log();
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+// console.log(undefined==0);
+
+// console.log(undefined>0);
+
+// console.log(undefined<=0);
+
+//=== this is equality operator this is stricly check the data type 
+
+console.log("2"===1);
+
+
+
+
+
