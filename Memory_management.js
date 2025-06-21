cb05da2e3b96981360bfabd5d7a1ee1082e87916
imsonukalwar <imsonukalwar@gmail.com>
2025-06-21 21:34:34 +0530

    Add files via upload

---
+++ Memory_management.js
@@ -0,0 +1,26 @@
+//stack(primitive data type)
+// heep(non primitive data type)
+
+let myyoutubename="sonukumar@gmail.com"
+let anothername=myyoutubename
+anothername="sonubbhai"
+
+
+// console.log(anothername);
+// console.log(myyoutubename);
+
+
+let userone={
+email="sonu@gmail.com",
+upi:"user@byl"
+}
+
+let usertwo=userone
+usertwo.email="monu@gmail.com"
+
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
