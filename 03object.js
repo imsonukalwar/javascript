fa50c2fb4b487833f05e07e9f15e8924e0bb6c71
imsonukalwar <imsonukalwar@gmail.com>
2025-06-27 21:20:37 +0530

    Add files via upload

---
+++ 03object.js
@@ -0,0 +1,13 @@
+const course={
+    coursename:"js with chai aur code",
+    prise:"999",
+    courseinstructor:"hitesh"
+}
+
+//course.courseinstructor
+
+const {courseinstructor:inspector}=course
+//console.log(courseinstructor);
+console.log(inspector);
+
+
